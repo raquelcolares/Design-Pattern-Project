7cc3ec702f66e99f561fc8f786bae18113a77fe8
Peluzo <jvpeluzo@gmail.com>
2024-07-18 18:42:40 -0400

    Csv Reader

---
+++ src/csv_reader.py
@@ -0,0 +1,11 @@
+import csv
+
+
+class CsvReader:
+    def read_csv_data(self, file_path: str) -> dict:
+        data = {}
+        with open(file_path, mode="r") as file:
+            csv_reader = csv.DictReader(file)
+            for row in csv_reader:
+                data[row["id"]] = row
+        return data
