9c9dfb7a441252c89ee5e8b962df23dcf8b2f628
Peluzo <jvpeluzo@gmail.com>
2024-07-19 19:37:20 -0400

    changes in json_reader and csv

--- src/csv_reader.py
+++ src/csv_reader.py
@@ -3,15 +3,22 @@ from .logger import MyLogger, LoggerAdapter, FileAdapter
 
 
 class CsvReader:
+    def __init__(self):
+        self.logger = LoggerAdapter(
+            MyLogger("CsvReader"), FileAdapter("csv_reader.log")
+        )
+
     def read_csv_data(self, file_path):
         """Read CSV data from file path"""
         data = []
-        self.logger = LoggerAdapter(
-            MyLogger("CsvAdapter"), FileAdapter("csv_adapter.log")
-        )
-        with open(file_path, newline="", encoding="utf-8") as file:
-            reader = csv.reader(file)
-            for row in reader:
-                data.append(row)
-
-        return data
+        self.logger.info(f"Reading CSV data from {file_path}")
+        try:
+            with open(file_path, newline="", encoding="utf-8") as file:
+                reader = csv.reader(file)
+                for row in reader:
+                    data.append(row)
+                self.logger.info(f"Successfully read {len(data)} rows from CSV file.")
+            return data
+        except FileNotFoundError as e:
+            self.logger.error(f"Error: CSV file not found - {e}")
+            return None  # Or raise an exception if appropriate
--- src/json_reader.py
+++ src/json_reader.py
@@ -3,11 +3,21 @@ from .logger import MyLogger, LoggerAdapter, FileAdapter
 
 
 class JsonReader:
-    def read_json_data(self, file_path):
+    def __init__(self):
         self.logger = LoggerAdapter(
-            MyLogger("CsvAdapter"), FileAdapter("csv_adapter.log")
+            MyLogger("JsonReader"), FileAdapter("json_reader.log")
         )
+
+    def read_json_data(self, file_path):
         """Read JSON data from file path"""
-        with open(file_path, mode="r", encoding="utf-8") as file:
-            data = json.load(file)
-        return data
+        self.logger.info(f"Reading JSON data from {file_path}")
+        try:
+            with open(file_path, mode="r", encoding="utf-8") as file:
+                data = json.load(file)
+                self.logger.info(
+                    f"Successfully read {len(data)} records from JSON file."
+                )
+            return data
+        except json.JSONDecodeError as e:
+            self.logger.error(f"Error decoding JSON: {e}")
+            return None
