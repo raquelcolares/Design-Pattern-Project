ccef24c5088bca0c390c1f8d72452a33f21fdef3
Raquel Colares <raquelcolaress@gmail.com>
2024-07-25 19:48:12 -0400

    updating unittest - adding logger testing

--- tests/tests.py
+++ tests/tests.py
@@ -1,13 +1,15 @@
-import unittest
 from data_loader import AdaptorFactory, CsvAdapter, JsonAdapter
+from subscribers import FileSubscriber
+from logger import *
 import pandas as pd
-
+import unittest
+from unittest.mock import patch
+from io import StringIO
 
 
 # First Test:
 ## Singleton 
 class TestSingletonDataLoader(unittest.TestCase):
-
     def test_singleton_data_loader(self):
         # Action
         s1_adapter = AdaptorFactory()
@@ -17,7 +19,7 @@ class TestSingletonDataLoader(unittest.TestCase):
 
 
 # Second Test:
-## Adaptor 
+## Adaptor and Factory
 
 class TestAdapters(unittest.TestCase):
     def test_csv_adapter(self):
@@ -33,5 +35,41 @@ class TestAdapters(unittest.TestCase):
         self.assertEqual(data_json.shape, expected_shape)
 
 
+# Third Test:
+## Observer
+
+class TestLogger(unittest.TestCase):
+    # 1st
+    def test_logger_consoleAdaptor(self):
+        log_console = MyLogger("Test1")
+        with patch("sys.stdout", new = StringIO()) as console_out:
+            log_console.info("Message 1")
+            self.assertEqual("[INFO] Test1: Message 1\n", console_out.getvalue())
+
+    # 2nd    
+    def test_logger_fileAdaptor(self):
+        file_adapter = FileAdapter("Test_file.txt")
+        log_file = MyLogger("Test2", "INFO", file_adapter)
+        log_file.info("Message 2")
+        with open("Test_file.txt", "r") as f:
+            for line in f:
+                written_string = line        
+        self.assertEqual("[INFO] Test2: Message 2\n", line)
+
+    # 3rd
+    def test_logger_observer(self):
+        logger = MyLogger("Test3")
+        logger_subscriber = FileSubscriber()
+        logger.register_WarningSubscriber(logger_subscriber)
+        logger.warning("Warning message test")
+        with open("logger_subscriber.txt", "r") as f:
+            for line in f:
+                written_string = line        
+        self.assertEqual("[WARNING] Test3: Warning message test\n", line)
+
+        
+
 if __name__ == "__main__":
     unittest.main()
+
+
